243f5c1196fa6eae5c3242f7ad0e9b28aa1ef999
PovilasKorop <povilask007@gmail.com>
2022-04-12 10:59:20 +0300

    Factories and seeds

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,16 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        $this->call(AdminSeeder::class);
+        \App\Models\User::factory(1)->create(['is_admin' => true]);
     }
 }
